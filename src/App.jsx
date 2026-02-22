708dd78b873c504e0e4ed907c904f9f05430d47d
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-02-22 19:41:25 +0530

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-center mt-10'><h1 className='text-red-900'>hello</h1></div>
+    </>
+  )
+}
+
+export default App
