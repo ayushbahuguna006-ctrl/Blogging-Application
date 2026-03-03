9994650f632a8262dae6d6125c69133d8b4c71a3
Ayush <ayushbahuguna006@gmail.com>
2026-03-03 21:59:00 +0530

    Backend-Route Setup comepleted

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -1,50 +1,100 @@
-const User=require("../models/usermodel")
+const User = require("../models/usermodel")
 const bcrypt = require('bcrypt');
-
-const register=async(req,res)=>{
+const jwt = require("jsonwebtoken")
+const register = async (req, res) => {
     try {
-        const{firstname,lastname,email,password}=req.body;
-        if(!firstname || !lastname || !email || !password){
+        const { firstname, lastname, email, password } = req.body;
+        if (!firstname || !lastname || !email || !password) {
             return res.status(400).json({
-                success:false,
-                message:'All fields are required'
+                success: false,
+                message: 'All fields are required'
             })
         }
-         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-            if(!emailRegex.test(email)){
-                return res.status(400).json({
-                    success:false,
-                    message:"Invalid email"
-                })
-            }
-            if(password.length<6){
-                return res.status(400).json({
-                    success:false,
-                    message:"password must be atleast 6 characters"
-                })
-            }
-            const existinguserbyemail=await User.findOne({email:email})
-            if(existinguserbyemail){
-                return res.status(400).json({
-                    success:false,
-                    message:'Email already exists'
-                })
-            }
-                const hashedPassword = await bcrypt.hash(password, 10);
-            await User.create({
-                firstname,
-                lastname,
-                email,
-                password:hashedPassword  })
-            return res.status(201).json({
-                success:true,
-                message:"Account created successfully"
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email"
             })
+        }
+        if (password.length < 6) {
+            return res.status(400).json({
+                success: false,
+                message: "password must be atleast 6 characters"
+            })
+        }
+        const existinguserbyemail = await User.findOne({ email: email })
+        if (existinguserbyemail) {
+            return res.status(400).json({
+                success: false,
+                message: 'Email already exists'
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword
+        })
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully"
+        })
     } catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Failed to register"
+            success: false,
+            message: "Failed to register"
         })
     }
 }
-module.exports=register
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'All fields are required'
+            })
+        }
+        let user =await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password"
+            })
+        }
+        const ispasswordvalid = await bcrypt.compare(password, user.password)
+        if (!ispasswordvalid) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            })
+        }
+        const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: "1d" })
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpOnly:true,sameSite:"strict"}).json({
+            success:true,
+            message:`Welcome back ${user.firstname}`,
+            user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to Login"
+        })
+    }
+}
+const logout=async (_,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            success:true,
+            message:"Logout Successful"
+        })
+    } catch (error) {
+        return res.json({
+            success:false,
+            message:"Logout was Unsuccessful"
+        })
+    }
+}
+module.exports = {register,login,logout}
--- Backend/routes/userroute.js
+++ Backend/routes/userroute.js
@@ -1,5 +1,7 @@
 const express=require('express')
-const register=require('../controllers/usercontroller')
+const {register,login,logout}=require('../controllers/usercontroller')
 const router=express.Router()
 router.post('/register',register)
+router.post('/login',login)
+router.get('/logout',logout)
 module.exports=router
