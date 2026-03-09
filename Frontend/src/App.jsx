f36eff0cf7eb9f402dd4e8d1924b53fedc685931
Ayush <ayushbahuguna006@gmail.com>
2026-03-09 18:05:19 +0530

    Hero section loaded in Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -22,7 +21,7 @@ function App() {
           <Route path="/login" element={<><Navbar/><Login /></>} />
           <Route path="/signup" element={<><Navbar/><Signup /></>} />
         </Routes>
-      </BrowserRouter>,
+      </BrowserRouter>
     </>
   )
 }
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useState } from 'react';
+import { FaFeatherAlt } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { MdDarkMode } from "react-icons/md";
 
 function Navbar() {
+  const user = false;
+  const [dark, setDark] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <>
+      <div className='flex items-center justify-between   md:justify-around bg-white h-[10vh]'>
+        <div>
+          <div className='ml-1'><FaFeatherAlt size={30} className='text-black'/></div>
+        </div>
+        <div className='hidden md:flex rounded-xl  border border-black'>
+          <div>
+            <input type="text" placeholder='Search' className='pl-5 border-l-5 outline-none w-[50vh] border-black rounded-l-md  p-1' />
+          </div>
+          <div>
+            <FaSearch size={34} className='bg-black w-10 p-2 text-white rounded-xl' />
+          </div>
+        </div>
+        <div className='flex'>
+          <Link to="/" className=' text-black  rounded-sm py-2 px-2.5 font-semibold hover:text-gray-500  hover:bg-white hover:text-black transition-all ease-in'>Home</Link>
+          <Link to="/blogs" className='text-black  rounded-sm py-2 px-2.5 font-semibold hover:text-gray-500  hover:bg-white   transition-all ease-in'>Blogs</Link>
+          <Link to="/about" className='text-black  rounded-sm py-2 px-2.5  font-semibold hover:text-gray-500  hover:bg-white  transition-all ease-in'>About</Link>
+        </div>
+        <div onClick={() => {
+    setDark(!dark)
+    document.body.style.backgroundColor = dark ? "white" : "black"
+    document.body.style.color = dark ? "black" : "white"
+  }}
+  className={`mt-1 border border-black rounded-sm cursor-pointer transition-all ease-in 
+  ${dark ? "bg-black text-white" : "null"}`}
+>
+          <MdDarkMode size={28} />
+        </div>
+        {user ? (null) : (<div className='flex gap-2 md:gap-6 justify-between items-center mt-1'>
+          <Link to="/login" className='text-white text-base  border-black rounded-sm md:py-0.5  py-1  px-2 h-20px md:px-2.5 bg-black font-bold hover:bg-white hover:text-black hover:border-black hover:border transition-all ease-in mr-1'>Login</Link>
+          <Link to="/signup" className='md:text-white hidden md:block text-black bg-stone-200 border-black text-base rounded-sm md:py-0.5   px-2 h-20px md:px-2 md:bg-black font-bold  hover:bg-white hover:text-black hover:border-black hover:border transition-all ease-in'>Signup</Link>
+        </div>)}
+      </div>
+      <hr />
+    </>
   )
 }
 
