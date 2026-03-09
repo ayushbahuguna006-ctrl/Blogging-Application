8f1eed42d08d64c0aaa21ac0769f462ea2b8d6f0
Ayush <ayushbahuguna006@gmail.com>
2026-03-09 22:28:38 +0530

    Pre Integration Signup Layout Completed

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import signup from '../assets/signup.jpg'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import NotificationProvider, { useNotification } from 'use-toast-notification'
 
 function Signup() {
+  const navigate=useNavigate();
+  const [user,setuser]=useState({
+    firstname:"",
+    lastname:"",
+    email:"",
+    password:""
+  })
+  const handlechange=(e)=>{
+    const {name,value}=e.target;
+    setuser((prev)=>({...prev,[name]:value})
+    )
+  }
+  const handlesubmit=async(e)=>{
+    e.preventDefault();
+    console.log(user);
+     try {
+      const res=await axios.post('http://localhost:8000/api/v1/user/register',user,{
+        headers:{
+          "Content-Type":"application/json"
+        },
+        withCredentials:true
+      })
+      if(res.data.success){
+        navigate('/login')
+      }
+      notification.show({
+            message: 'Signed Up Succesfully', 
+            title: 'Delivery Status',
+            variant: 'success'
+        })
+     } catch (error) {
+      console.log(error);
+      notification.show({
+            message: 'Seems like an Error', 
+            title: 'Status',
+            variant: 'error'
+        })
+     }
+    
+  }
   return (
     <>
       <div className='flex justify-around items-center'>
@@ -15,17 +56,17 @@ function Signup() {
               <div className='text-center'><h1 className='text-2xl font-medium mt-5 md:mt-0'>Create an Account</h1></div>
               <div className='text-center'><h3>Enter your Details below to create your account</h3></div>
               <div className='flex flex-col md:flex-row gap-5 text-center md:text-start mt-5'>
-                <label><h5 className='font-bold'>First Name</h5><input type="text" placeholder='First Name' className='border w-[85vw] outline-none md:w-[15vw] p-2 border-black rounded-xl' /></label>
-                <label><h5 className='font-bold'>Last Name</h5><input type="text" placeholder='Last Name' className='border w-[85vw] outline-none md:w-[15vw] p-2 border-black rounded-xl' /></label>
+                <label><h5 className='font-bold'>First Name</h5><input type="text" onChange={handlechange} name='firstname' placeholder='First Name' className='border w-[85vw] outline-none md:w-[15vw] p-2 border-black rounded-xl' /></label>
+                <label><h5 className='font-bold'>Last Name</h5><input type="text"  onChange={handlechange} name='lastname' placeholder='Last Name' className='border w-[85vw] outline-none md:w-[15vw] p-2 border-black rounded-xl' /></label>
               </div>
               <div className='text-center md:text-start mt-5'>
-                <label><h5 className='font-bold'>Email</h5><input type="text" placeholder='John.Doe@example.com' className='border w-[85vw] outline-none md:w-[31.5vw] p-2 border-black rounded-xl' /></label>
+                <label><h5 className='font-bold'>Email</h5><input type="text"  onChange={handlechange} name='email' placeholder='John.Doe@example.com' className='border w-[85vw] outline-none md:w-[31.5vw] p-2 border-black rounded-xl' /></label>
               </div>
               <div className='text-center md:text-start mt-5'>
-                <label><h5 className='font-bold'>Password</h5><input type="password" placeholder='Create a Password' className='border w-[85vw] outline-none md:w-[31.5vw] p-2 border-black rounded-xl' /></label>
+                <label><h5 className='font-bold'>Password</h5><input type="password"  onChange={handlechange} name='password' placeholder='Create a Password' className='border w-[85vw] outline-none md:w-[31.5vw] p-2 border-black rounded-xl' /></label>
               </div>
               <div className='mt-5 text-center flex flex-col'>
-                <button className='bg-gradient-to-r from-indigo-500 via-purple-500 cursor-pointer to-pink-500 p-2 w-[85vw] rounded-xl text-white md:w-[30vw] font-medium'>Sign Up</button>
+                <button onClick={handlesubmit} className='bg-gradient-to-r from-indigo-500 via-purple-500 cursor-pointer to-pink-500 p-2 w-[85vw] rounded-xl text-white md:w-[30vw] font-medium'>Sign Up</button>
                 <span className='mt-4'>Already have an account?  <Link className='text-blue-500' to="/Login">SIGN-IN</Link></span>
               </div>
             </div>
