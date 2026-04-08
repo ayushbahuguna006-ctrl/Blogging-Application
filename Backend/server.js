b5f0ce431f7f61498fa362e38cc62c34d0f80e48
Ayush <ayushbahuguna006@gmail.com>
2026-04-08 10:34:05 +0530

    [Backend]-Port Configurations

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,6 @@ app.use(cors({
   credentials: true,
 }))
 app.use('/api/v1/user',userroute)
-const PORT=process.env.PORT 
+const PORT=process.env.PORT || 8000
 connectDB()
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
